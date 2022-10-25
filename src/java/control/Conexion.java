1a78b87bc0541b2515cae73d77d6ab90ded9b554
Maver <M4ver>
2022-10-25 10:03:27 -0500

    Creando Conexion

---
+++ src/java/control/Conexion.java
@@ -0,0 +1,32 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion 
+{
+    
+    private static Connection conecta;
+    private static final String DRIVER="org.postgresql.Driver";
+    private static final String URL="jdbc:postgresql://localhost:5432/bd_alumnos";
+    private static final String USER="postgres";
+    private static final String PASSWORD="root";
+    
+    public static Connection conectar()
+    {
+        try 
+        {
+            Class.forName(DRIVER);
+            conecta=DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa");
+        } catch (Exception e) 
+        {
+            e.printStackTrace(System.out);
+        }
+        return conecta;
+    }
+    public static void main(String []args)
+    {
+        Conexion.conectar();
+    }
+}
